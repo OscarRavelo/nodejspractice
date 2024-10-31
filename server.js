df513221acd3a8b2a19e9d3afde62e15892debd9
Oscar Ravelo <oscar91ravelo@gmail.com>
2024-10-31 09:25:04 +0100

    adding friends endpoint and query params friendid

--- server.js
+++ server.js
@@ -4,6 +4,12 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+const friends = [
+  { id: 1, name: "Alice" },
+  { id: 2, name: "Bob" },
+  { id: 3, name: "Charlie" },
+];
+
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
@@ -12,9 +18,21 @@ app.get("/about", (req, res) => {
   res.send("About Page");
 });
 
-app.post("messages", (req, res) => {
-  console.log("Updating messages....");
+app.get("/friends", (req, res) => {
+  res.send(friends);
 });
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
+});
+
+app.get("/friends/:id", (req, res) => {
+  const friendId = Number(req.params.id);
+  console.log(friendId);
+  const friend = friends.find((friend) => friend.id === friendId);
+  if (!friend) {
+    res.status(404).send("Friend not found");
+  } else {
+    res.send(friend);
+  }
 });
