43ce05eda10e3d1c31179d113ddf530152303080
Oscar Ravelo <oscar91ravelo@gmail.com>
2024-10-31 10:10:03 +0100

    adding post method and setting the server to work only with json

--- server.js
+++ server.js
@@ -3,12 +3,33 @@ const express = require("express");
 const app = express();
 
 const PORT = process.env.PORT || 3000;
-
 const friends = [
   { id: 1, name: "Alice" },
   { id: 2, name: "Bob" },
   { id: 3, name: "Charlie" },
 ];
+
+app.use((req, res, next) => {
+  const start = Date.now();
+  console.log(`${req.method} ${req.url}`);
+  next();
+  const delta = Date.now() - start;
+  console.log(`Request took: ${delta}ms`);
+});
+
+// setting express to use json
+app.use(express.json());
+
+// post  request
+
+app.post("/friends", (req, res) => {
+  const Newfriend = {
+    id: friends.length + 1,
+    name: req.body.name,
+  };
+  friends.push(Newfriend);
+  res.send(Newfriend);
+});
 
 app.get("/", (req, res) => {
   res.send("Hello World");
